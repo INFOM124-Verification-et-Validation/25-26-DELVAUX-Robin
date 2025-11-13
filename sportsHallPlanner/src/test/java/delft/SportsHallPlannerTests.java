c306cd5a6e245bc7b17972f9163346d14c57e518
Robin23D <robin.delvaux@student.unamur.be>
2025-11-13 10:55:26 +0100

    quelques tests pour SportsHallPlanner

--- sportsHallPlanner/src/test/java/delft/SportsHallPlannerTests.java
+++ sportsHallPlanner/src/test/java/delft/SportsHallPlannerTests.java
@@ -15,4 +15,48 @@ import static delft.SportsHallPlanner.planHalls;
 
 class SportsHallPlannerTests {
 
+    List<SportsHall> halls = new ArrayList<>();
+    List<Request> requests = new ArrayList<>();
+
+    @Test
+    void happyPathTest() {
+        Set<Property> properties = new HashSet<>();
+        properties.add(NEAR_CITY_CENTRE);
+        Request request = new Request(properties, BADMINTON, 1);
+        requests.add(request);
+
+        Set<Property> properties2 = new HashSet<>();
+        properties2.add(NEAR_CITY_CENTRE);
+        Map<Field, Integer> fields = new HashMap<>();
+        fields.put(BADMINTON, 2);
+        SportsHall hall = new SportsHall(properties, fields);
+        halls.add(hall);
+
+        Map<SportsHall, Request> planning = SportsHallPlanner.planHalls(requests, halls);
+
+        assertThat(planning.get(hall)).isEqualTo(request);
+    }
+
+    @Test
+    void TwoRequestsTest() {
+        Set<Property> properties = new HashSet<>();
+        properties.add(NEAR_CITY_CENTRE);
+        Request request1 = new Request(properties, BADMINTON, 2);
+        Request request2 = new Request(properties, BADMINTON, 1);
+        requests.add(request1);
+        requests.add(request2);
+
+        Set<Property> properties2 = new HashSet<>();
+        properties2.add(NEAR_CITY_CENTRE);
+        Map<Field, Integer> fields = new HashMap<>();
+        fields.put(BADMINTON, 1);
+        SportsHall hall1 = new SportsHall(properties, fields);
+        SportsHall hall2 = new SportsHall(properties, fields);
+        halls.add(hall2);
+
+        Map<SportsHall, Request> planning = SportsHallPlanner.planHalls(requests, halls);
+
+        assertThat(planning.get(hall1)).isEqualTo(request2);
+        assertThat(planning.get(hall2)).isEqualTo(request1);
+    }
 }
